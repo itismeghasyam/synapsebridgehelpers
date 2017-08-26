482fbca8e2fe90a02f2a6c80233f0fb02086c8ca
Meghasyam Tummalacherla <itismeghasyam@gmail.com>
2017-08-26 11:50:27 -0700

    Update summaryTable.py
    Added two columns originalTableName and originalTableId

--- synapsebridgehelpers/summaryTable.py
+++ synapsebridgehelpers/summaryTable.py
@@ -26,5 +26,8 @@ def summaryTable(syn, projectId, columns = []):
     for table_id in all_tables['table.id']:
         df = syn.tableQuery('select ' +columns_str+' from '+ table_id)
         df = df.asDataFrame()
+        schema = syn.get(table_id)
+        df['originalTableName'] = [schema.name for count in range(0,df.shape[0])]
+        df['originalTableId'] = [schema.id for count in range(0,df.shape[0])]
         df_main = pd.concat([df_main,df])
     return df_main
